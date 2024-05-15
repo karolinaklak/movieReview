20f542e21c015f26fc0f223231e2ac4565f8e016
karolinaklak <karolinaklak@live.fr>
2024-05-15 10:32:24 +0200

    update README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,9 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +25,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule,
   ],
   providers: [
     provideClientHydration()
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  trendingMovies: any;
+
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+      this.getTrendingMovies();
+  }
+
+  getTrendingMovies(){
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) => {
+      this.trendingMovies=movies;
+      console.log(this.trendingMovies);
+    });
+  }
 }
